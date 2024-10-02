140d4623b4f2dd02834a2023f68694bc91c1ab70
Taghi Eynullaev <55533028+Sekerator@users.noreply.github.com>
2024-10-02 18:18:38 +0300

    Guse title add

--- frontend/views/site/guse.php
+++ frontend/views/site/guse.php
@@ -1,9 +1,20 @@
 <?php $this->title = 'Guse' ?>
-<div></div>
+<div id="guse"></div>
 <style>
-    div {
+    #guse {
+        position: absolute;
+        left: 0;
+        top: 0;
         width: 5000px !important;
         height: 5000px !important;
         background-image: url("../frontend/web/images/guse.gif")
     }
 </style>
+
+<script>
+    for(var i = 0; i < 1000000; i++) {
+        document.getElementById('guse').left = 0;
+        document.getElementById('guse').left -= 10;
+        document.getElementById('guse').width += 10;
+    }
+</script>
