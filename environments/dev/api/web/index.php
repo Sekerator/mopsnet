92236083b45ce8a2ee74829c1915d20de6be2e82
Taghi Eynullaev <55533028+Sekerator@users.noreply.github.com>
2024-10-02 18:27:12 +0300

    Edit environments

--- environments/dev/api/web/index.php
+++ environments/dev/api/web/index.php
@@ -4,7 +4,8 @@ defined('YII_ENV') or define('YII_ENV', 'dev');
 
 require(__DIR__ . '/../../vendor/autoload.php');
 require(__DIR__ . '/../../vendor/yiisoft/yii2/Yii.php');
-require(__DIR__ . '/../../common/config/aliases.php');
+require(__DIR__ . '/../../common/config/bootstrap.php');
+require __DIR__ . '/../config/bootstrap.php';
 
 $config = yii\helpers\ArrayHelper::merge(
     require(__DIR__ . '/../../common/config/main.php'),
@@ -13,5 +14,5 @@ $config = yii\helpers\ArrayHelper::merge(
     require(__DIR__ . '/../config/main-local.php')
 );
 
-$application = new yii\web\Application($config);
-$application->run();
+(new yii\web\Application($config))->run();
+
